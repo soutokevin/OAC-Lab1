ebc2ecbd3b0aea35387334c21dc1d83126570244
dieg0d <diegodi05@hotmail.com>
2019-05-01 17:47:24 +0000

    imprime linhas no bitmap

--- line.asm
+++ line.asm
@@ -8,16 +8,18 @@
 # a1 = y0
 # a2 = x1
 # a3 = y1
+# a4 = cor
 # t0 = abs(x1 - x0)
 # t1 = abs(y1 - y0)
 # t2 = x0 < x1 ? 1 : -1
 # t3 = y0 < y1 ? 1 : -1
 # t4 = (abs(x1 - x0) > abs(y1 - y0) ? abs(x1 - x0) : - abs(y1 - y0))/2
 
-	li a0 0
-	li a1 0
-	li a2 3
-	li a3 3
+	li a0 86
+	li a1 20
+	li a2 150
+	li a3 62
+	li a4 0x07
 Line: 
 	sub t0 a2 a0  			# subtrai x1 - x0
 	ABS(t0)							# pega o valor absoluto dessa subtração
@@ -45,17 +47,7 @@ atribui:
 continua3:
 
 loop:
-	li a7 1
-	ecall 
-	mv tp a0
-	mv a0 a1
-	ecall
-	mv a0 tp
-	li a7 11
-	mv tp a0
-	li a0 10
-	ecall
-	mv a0 tp
+	jal paint_pixel
 	bne a0 a2 calcula
 	bne a1 a3 calcula 
 	j exit
@@ -75,3 +67,13 @@ exit:
 
  li a7 10
  ecall
+
+
+paint_pixel:  
+  li s1 320
+  mul s1 a1 s1
+  add s1 s1 a0
+  li s2 0xFF000000
+  add s1 s1 s2
+  sb a4 (s1)
+  ret
