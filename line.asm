5b2fd45e2b65128b33026257a7399c6c5662b87f
Kevin Souto <kevin@resavek.home>
2019-05-05 09:51:01 -0300

    format code

--- line.asm
+++ line.asm
@@ -1,81 +1,67 @@
-.text
+										# fn (u32, u32, u32, u32, u8)
+										# a0 = x0
+										# a1 = y0
+										# a2 = x1
+										# a3 = y1
+										# a4 = cor
+line:               addi sp sp -24
+					          sw ra  0(sp)
+					          sw s1  4(sp)                  # s1 = abs(x1 - x0)
+					          sw s2  8(sp)                  # s2 = abs(y1 - y0)
+					          sw s3 12(sp)                  # s3 = x0 < x1 ? 1 : -1
+					          sw s4 16(sp)                  # s4 = y0 < y1 ? 1 : -1
+					          sw s5 20(sp)                  # s5 = (s1 > s2 ? s1 : -s2) / 2
 
-# a0 = x0
-# a1 = y0
-# a2 = x1
-# a3 = y1
-# a4 = cor
-# s1 = abs(x1 - x0)
-# s2 = abs(y1 - y0)
-# s3 = x0 < x1 ? 1 : -1
-# s4 = y0 < y1 ? 1 : -1
-# s5 = (abs(x1 - x0) > abs(y1 - y0) ? abs(x1 - x0) : - abs(y1 - y0))/2
+					          sub s1 a2 a0  			          # subtrai x1 - x0
+					          ABS(s1)							          # pega o valor absoluto dessa subtração
+					          sub s2 a3 a1 	 			          # subtrai y1 - y0
+					          ABS(s2)       			          # pega o valor absoluto dessa subtração
 
-line:
-	addi sp sp -24
-	sw ra  0(sp)
-	sw s1  4(sp)
-	sw s2  8(sp)
-	sw s3 12(sp)
-	sw s4 16(sp)
-	sw s5 20(sp)
+					          # s3 = a0 < a2 ? 1 : -1
+					          slt s3 a0 a2
+					          slli s3 s3 1
+					          addi s3 s3 -1
 
-	sub s1 a2 a0  			# subtrai x1 - x0
-	ABS(s1)							# pega o valor absoluto dessa subtração
-	sub s2 a3 a1 	 			# subtrai y1 - y0
-	ABS(s2)       			# pega o valor absoluto dessa subtração
+					          # s4 = a1 < a2 ? 1 : -1
+					          slt s4 a1 a3
+					          slli s4 s4 1
+					          addi s4 s4 -1
 
-	# s3 = a0 < a2 ? 1 : -1
-	slt s3 a0 a2
-	slli s3 s3 1
-	addi s3 s3 -1
+					          bgt s1 s2  atribui            # abs(x1 - x0) > abs(y1 - y0) ?
+					          sub s5 zero s2			          # - abs(y1 - y0) para deixar negativo
+					          srai s5 s5 1 				          # divide - abs(y1 - y0) por 2
+					          j loop1
 
-	# s4 = a1 < a2 ? 1 : -1
-	slt s4 a1 a3
-	slli s4 s4 1
-	addi s4 s4 -1
+atribui:            srai s5 s1 1                  # divide abs(x1 - x0) por 2
 
-	bgt s1 s2  atribui  # abs(x1 - x0) > abs(y1 - y0) ?
-	sub s5 zero s2			# - abs(y1 - y0) para deixar negativo
-	srai s5 s5 1 				# divide - abs(y1 - y0) por 2
-	j loop1
+loop1:              call pixel
+					          bne a0 a2 calcula
+					          bne a1 a3 calcula
 
-atribui:
-	srai s5 s1 1        # divide abs(x1 - x0) por 2
+                    lw s5 20(sp)
+					          lw s4 16(sp)
+					          lw s3 12(sp)
+					          lw s2  8(sp)
+					          lw s1  4(sp)
+					          lw ra  0(sp)
+					          addi sp sp 24
+					          ret
 
-loop1:
-	jal pixel
-	bne a0 a2 calcula
-	bne a1 a3 calcula
-	j exit
-calcula:
-	mv t5 s5
-	sub t6 zero s1
-	ble t5 t6 proximo
-	sub s5 s5 s2
-	add a0 a0 s3
-proximo:
-	bge t5 s2 loop1
-	add s5 s5 s1
-	add a1 a1 s4
-	j loop1
+calcula:            mv t5 s5
+					          sub t6 zero s1
+					          ble t5 t6 proximo
+					          sub s5 s5 s2
+					          add a0 a0 s3
 
-exit:
-	lw s5 20(sp)
-	lw s4 16(sp)
-	lw s3 12(sp)
-	lw s2  8(sp)
-	lw s1  4(sp)
-	lw ra  0(sp)
-	addi sp sp 24
-  ret
+proximo:            bge t5 s2 loop1
+					          add s5 s5 s1
+					          add a1 a1 s4
+					          j loop1
 
-
-pixel:
-  li t0 320
-  mul t0 a1 t0
-  add t0 t0 a0
-  li t1 0xFF000000
-  add t0 t0 t1
-  sb a4 (t0)
-  ret
+pixel:              li t0 320
+					          mul t0 a1 t0
+					          add t0 t0 a0
+					          li t1 0xFF000000
+					          add t0 t0 t1
+					          sb a4 (t0)
+					          ret
--- main.asm
+++ main.asm
@@ -1,4 +1,5 @@
 .data
+
 N: .word 6
 C: .space 160
 D: .space 1600
@@ -9,135 +10,135 @@ D: .space 1600
 .include "macros.asm"
 M_SetEcall(exceptionHandling)
 
-main:     la t0 N
-          lw a0 (t0)
-          la a1 C
-          call sorteio
+main:               la t0 N
+                    lw a0 (t0)
+                    la a1 C
+                    call sorteio
 
-          li a0 0xff
-          call paint
+                    li a0 0xff
+                    call paint
 
-          la t0 N
-          lw a0 (t0)
-          la a1 C
-          call rotas
+                    la t0 N
+                    lw a0 (t0)
+                    la a1 C
+                    call rotas
 
-          la t0 N
-          lw a0 (t0)
-          la a1 C
-          call desenha
+                    la t0 N
+                    lw a0 (t0)
+                    la a1 C
+                    call desenha
 
-          li a7 10
-          ecall
+                    li a7 10
+                    ecall
 
-# fn (i32, [i32, i32]) -> ()
-# a0: number of elements
-# a1: pointer to the elements
-sorteio:  slli t0 a0 1            # multiply counter by 2 (each element is 64b)
-          mv t2 a1                # save base address
-          li a7 42                # random number syscall code (won't change)
+                    # fn (u32, [u32, u32])
+                    # a0: number of elements
+                    # a1: pointer to the elements
+sorteio:            slli t0 a0 1                  # multiply counter by 2 (each element is 64b)
+                    mv t2 a1                      # save base address
+                    li a7 42                      # random number syscall code (won't change)
 
-loop:     addi t0 t0 -2           # decrement element count
-          slli t1 t0 2            # calculate address offset
-          add t1 t1 t2            # calculate adddress
+loop:               addi t0 t0 -2                 # decrement element count
+                    slli t1 t0 2                  # calculate address offset
+                    add t1 t1 t2                  # calculate adddress
 
-          li a0 0                 # reset seed selector
-          li a1 310               # set upper bound
-          ecall                   # generate random number
-          sw a0 0(t1)             # store random number
+                    li a0 0                       # reset seed selector
+                    li a1 310                     # set upper bound
+                    ecall                         # generate random number
+                    sw a0 0(t1)                   # store random number
 
-          li a0 0                 # reset seed selector
-          li a1 230               # set upper bound
-          ecall                   # generate random number
-          sw a0 4(t1)             # store random number
+                    li a0 0                       # reset seed selector
+                    li a1 230                     # set upper bound
+                    ecall                         # generate random number
+                    sw a0 4(t1)                   # store random number
 
-          bnez t0 loop            # are we done yet?
-          ret                     # yes we are!!
+                    bnez t0 loop                  # are we done yet?
+                    ret                           # yes we are!!
 
-# fn (i32, [i32, i32]) -> ()
-# a0: number of elements
-# a1: pointer to the elements
-desenha:  slli t0 a0 1            # multiply counter by 2 (each element is 64b)
-          mv t1 a1                # save base address
-          li a7 111               # print char on bitmap display code
+                    # fn (u32, [u32, u32])
+                    # a0: number of elements
+                    # a1: pointer to the elements
+desenha:            slli t0 a0 1                  # multiply counter by 2 (each element is 64b)
+                    mv t1 a1                      # save base address
+                    li a7 111                     # print char on bitmap display code
 
-loop2:    addi t0 t0 -2           # decrement element count
-          slli t2 t0 2            # calculate address offset
-          add t2 t1 t2            # calculate adddress
+loop2:              addi t0 t0 -2                 # decrement element count
+                    slli t2 t0 2                  # calculate address offset
+                    add t2 t1 t2                  # calculate adddress
 
-          srai a0 t0 1            # get element index
-          addi a0 a0 65           # calculate char
-          lw a1 0(t2)             # get x position
-          lw a2 4(t2)             # get y position
-          li a3 0x3800            # set color (black font on green bg)
-          li a4 0                 # use frame 0
-          ecall                   # print char
+                    srai a0 t0 1                  # get element index
+                    addi a0 a0 65                 # calculate char
+                    lw a1 0(t2)                   # get x position
+                    lw a2 4(t2)                   # get y position
+                    li a3 0x3800                  # set color (black font on green bg)
+                    li a4 0                       # use frame 0
+                    ecall                         # print char
 
-          bnez t0 loop2           # are we done yet?
-          ret                     # yes we are!!
+                    bnez t0 loop2                 # are we done yet?
+                    ret                           # yes we are!!
 
-# fn (u8) -> ()
-# a0: color used to paint the display
-paint:    li t0 0xff000000        # base display address
-          li t1 76800             # number of pixels on the display (320x240)
-          add t1 t0 t1            # end display address (base + pixels)
-          slli t2 a0 8
-          or a0 a0 t2
-          slli t2 a0 16
-          or a0 a0 t2
-loop3:    sw a0 (t0)              # paint pixel
-          addi t0 t0 4            # increment address
-          blt t0 t1 loop3         # are we done yet?
-          ret                     # yes we are!!
+                    # fn (u8)
+                    # a0: color used to paint the display
+paint:              li t0 0xff000000              # base display address
+                    li t1 76800                   # number of pixels on the display (320x240)
+                    add t1 t0 t1                  # end display address (base + pixels)
+                    slli t2 a0 8
+                    or a0 a0 t2
+                    slli t2 a0 16
+                    or a0 a0 t2
+loop3:              sw a0 (t0)                    # paint pixel
+                    addi t0 t0 4                  # increment address
+                    blt t0 t1 loop3               # are we done yet?
+                    ret                           # yes we are!!
 
-# fn (i32, [i32, i32]) -> ()
-# a0: number of elements
-# a1: pointer to the elements
-rotas:    addi sp sp 16
-          sw ra  0(sp)
-          sw s1  4(sp)
-          sw s2  8(sp)
-          sw s3 12(sp)
+                    # fn (u32, [u32, u32])
+                    # a0: number of elements
+                    # a1: pointer to the elements
+rotas:              addi sp sp 16
+                    sw ra  0(sp)
+                    sw s1  4(sp)
+                    sw s2  8(sp)
+                    sw s3 12(sp)
 
-          slli s1 a0 3            # get the size of the list (in bytes)
-          add s1 s1 a1            # final address of the list
-          mv s2 a1                # copy list address
+                    slli s1 a0 3                  # get the size of the list (in bytes)
+                    add s1 s1 a1                  # final address of the list
+                    mv s2 a1                      # copy list address
 
-loop4:    mv s3 s2                # element pointer for inner loop
+loop4:              mv s3 s2                      # element pointer for inner loop
 
-loop5:    lw a0 0(s2)             # x for current element from outer loop
-          lw a1 4(s2)             # y for current element from outer loop
-          lw a2 0(s3)             # x for current element from inner loop
-          lw a3 4(s3)             # y for current element from inner loop
-          li a4 0x00              # set line color (black)
-          call line
+loop5:              lw a0 0(s2)                   # x for current element from outer loop
+                    lw a1 4(s2)                   # y for current element from outer loop
+                    lw a2 0(s3)                   # x for current element from inner loop
+                    lw a3 4(s3)                   # y for current element from inner loop
+                    li a4 0x00                    # set line color (black)
+                    call line
 
-          addi s3 s3 8            # point to the next element
-          blt s3 s1 loop5         # inner loop check
+                    addi s3 s3 8                  # point to the next element
+                    blt s3 s1 loop5               # inner loop check
 
-          addi s2 s2 8            # increment outer loop counter
-          blt s2 s1 loop4         # outer loop check
+                    addi s2 s2 8                  # increment outer loop counter
+                    blt s2 s1 loop4               # outer loop check
 
-          lw s3 12(sp)
-          lw s2  8(sp)
-          lw s1  4(sp)
-          lw ra  0(sp)
-          addi sp sp 16
-          ret
+                    lw s3 12(sp)
+                    lw s2  8(sp)
+                    lw s1  4(sp)
+                    lw ra  0(sp)
+                    addi sp sp 16
+                    ret
 
-# a0: x
-# a1: y
-# a2: color
-# address = display + y * 320 + x = display + (y << 8) + (y << 6) + x
-paint_pixel:  li t0 0xff000000    # get base address for the display
-              slli t1 a1 8        # t1 = y * (2 ^ 8) = y << 8
-              slli t2 a1 6        # t2 = y * (2 ^ 6) = y << 6
-              add t1 t2 t1        # t1 = y * 320
-              add t1 t1 a0        # offset
-              add t0 t0 t1        # get final address
-              sb a2 (t0)          # paint pixel
-              ret
-
+                    # fn (u32, u32, u8)
+                    # a0: x
+                    # a1: y
+                    # a2: color
+                    # address = display + y * 320 + x = display + (y << 8) + (y << 6) + x
+paint_pixel:        li t0 0xff000000              # get base address for the display
+                    slli t1 a1 8                  # t1 = y * (2 ^ 8) = y << 8
+                    slli t2 a1 6                  # t2 = y * (2 ^ 6) = y << 6
+                    add t1 t2 t1                  # t1 = y * 320
+                    add t1 t1 a0                  # offset
+                    add t0 t0 t1                  # get final address
+                    sb a2 (t0)                    # paint pixel
+                    ret
 
 .include "SYSTEMv13.s"
 .include "line.asm"
