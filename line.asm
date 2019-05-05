056e11723d3509fe36981df39b4445044f1ab7f2
Kevin Souto <kevin@resavek.home>
2019-05-05 11:28:04 -0300

    don't need the stack

--- line.asm
+++ line.asm
@@ -4,37 +4,35 @@
 										# a2 = x1
 										# a3 = y1
 										# a4 = cor
-line:               addi sp sp -20
-					          sw s1  0(sp)                  # s1 = abs(x1 - x0)
-					          sw s2  4(sp)                  # s2 = abs(y1 - y0)
-					          sw s3  8(sp)                  # s3 = x0 < x1 ? 1 : -1
-					          sw s4 12(sp)                  # s4 = y0 < y1 ? 1 : -1
-					          sw s5 16(sp)                  # s5 = (s1 > s2 ? s1 : -s2) / 2
-					          li t0 0xFF000000
+					          # t2 = abs(x1 - x0)
+					          # t3 = abs(y1 - y0)
+					          # t4 = x0 < x1 ? 1 : -1
+					          # t5 = y0 < y1 ? 1 : -1
+					          # t6 = (t2 > t3 ? t2 : -t3) / 2
+line:               sub t2 a2 a0  			          # subtrai x1 - x0
+					          ABS(t2)							          # pega o valor absoluto dessa subtração
+					          sub t3 a3 a1 	 			          # subtrai y1 - y0
+					          ABS(t3)       			          # pega o valor absoluto dessa subtração
 
-					          sub s1 a2 a0  			          # subtrai x1 - x0
-					          ABS(s1)							          # pega o valor absoluto dessa subtração
-					          sub s2 a3 a1 	 			          # subtrai y1 - y0
-					          ABS(s2)       			          # pega o valor absoluto dessa subtração
+					          # t4 = a0 < a2 ? 1 : -1
+					          slt t4 a0 a2
+					          slli t4 t4 1
+					          addi t4 t4 -1
 
-					          # s3 = a0 < a2 ? 1 : -1
-					          slt s3 a0 a2
-					          slli s3 s3 1
-					          addi s3 s3 -1
+					          # t5 = a1 < a2 ? 1 : -1
+					          slt t5 a1 a3
+					          slli t5 t5 1
+					          addi t5 t5 -1
 
-					          # s4 = a1 < a2 ? 1 : -1
-					          slt s4 a1 a3
-					          slli s4 s4 1
-					          addi s4 s4 -1
-
-					          bgt s1 s2  atribui            # abs(x1 - x0) > abs(y1 - y0) ?
-					          sub s5 zero s2			          # - abs(y1 - y0) para deixar negativo
-					          srai s5 s5 1 				          # divide - abs(y1 - y0) por 2
+					          bgt t2 t3  atribui            # abs(x1 - x0) > abs(y1 - y0) ?
+					          sub t6 zero t3			          # - abs(y1 - y0) para deixar negativo
+					          srai t6 t6 1 				          # divide - abs(y1 - y0) por 2
 					          j loop1
 
-atribui:            srai s5 s1 1                  # divide abs(x1 - x0) por 2
+atribui:            srai t6 t2 1                  # divide abs(x1 - x0) por 2
 
-loop1:              li t1 320
+loop1:              li t0 0xFF000000
+										li t1 320
 										mul t1 a1 t1
 					          add t1 t1 a0
 					          add t1 t1 t0
@@ -42,23 +40,16 @@ loop1:              li t1 320
 
 					          bne a0 a2 calcula
 					          bne a1 a3 calcula
-
-                    lw s5 16(sp)
-					          lw s4 12(sp)
-					          lw s3  8(sp)
-					          lw s2  4(sp)
-					          lw s1  0(sp)
-					          addi sp sp 20
 					          ret
 
-calcula:            mv t5 s5
-					          sub t6 zero s1
-					          ble t5 t6 proximo
-					          sub s5 s5 s2
-					          add a0 a0 s3
+calcula:            mv t1 t6
+					          sub t0 zero t2
+					          ble t1 t0 proximo
+					          sub t6 t6 t3
+					          add a0 a0 t4
 
-proximo:            bge t5 s2 loop1
-					          add s5 s5 s1
-					          add a1 a1 s4
+proximo:            bge t1 t3 loop1
+					          add t6 t6 t2
+					          add a1 a1 t5
 					          j loop1
 
