75edf3bc3d106c6ac653ab87bfc66303c2de6e3a
Kevin Souto <soutokevin@gmail.com>
2019-05-07 19:33:59 -0300

    Inline swap function

--- permutation.asm
+++ permutation.asm
@@ -6,15 +6,21 @@ arrei:              .word 3 1 2
 
                     .text
 
-                    # a0 = n(size)
-                    # a1 = array of any
-                    li a0 3
+main:               li a0 3
                     la a1 arrei
+                    call permutation
 
+                    li a7 10
+                    ecall
+
+                    # fn (i32, [i32])
+                    # a0: number of elements on the array
+                    # a1: array of values to permutate
 permutation:        mv s0 a0                      # coloca size em s0
                     mv s1 a1                      # coloca array em s1
                     mv t3 s0                      # coloca size em t3 para fazer o loop do show
                     add t1 zero zero              # t1 = i
+
 olokinho:           mv a1 s1
 show:               li a7, 1                      # printa o rolê
                     lw a0 0(a1)
@@ -26,6 +32,7 @@ show:               li a7, 1                      # printa o rolê
                     li a7 11
                     ecall
                     mv t3 s0                      # coloca size em t3 para fazer o loop do show
+
 while:              bge t1 s0 exit                # se i >= 3 sai do loop
                     slli t4 t1 2                  # t4 = i * 4
                     la t0 array                   # pega o array da memoria
@@ -38,14 +45,22 @@ while:              bge t1 s0 exit                # se i >= 3 sai do loop
                     mv a0 s1                      # a0 = endereco de A[0]
                     slli t4 t1 2
                     add a1 s1 t4                  # a1 = endereco de A[i]
-                    jal swap
+                    lw t5 0(a0)
+                    lw t6 0(a1)
+                    sw t6 0(a0)
+                    sw t5 0(a1)
                     j continua
+
 impar:              slli t4 t4 2
                     add t4 s1 t4                  # t4 = A[c[i]]
                     mv a0 t4                      # a0 = endereco de A[c[i]]
                     slli t4 t1 2
                     add a1 s1 t4                  # a1 = endereco de A[i]
-                    jal swap
+                    lw t5 0(a0)
+                    lw t6 0(a1)
+                    sw t6 0(a0)
+                    sw t5 0(a1)
+
 continua:           slli t4 t1 2                  # t4 = i * 4
                     la t0 array                   # pega o array da memoria
                     add t0 t0 t4
@@ -54,11 +69,9 @@ continua:           slli t4 t1 2                  # t4 = i * 4
                     sw t4 0(t0)
                     add t1 zero zero
                     j olokinho
+
 pula:               sw zero 0(t0)
                     addi t1 t1 1
                     j while
-swap:               lw t5 0(a0)
-                    lw t6 0(a1)
-                    sw t6 0(a0)
-                    sw t5 0(a1)
+
 exit:               ret
